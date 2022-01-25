2dbbf4810818fbc2ff8b8de014506d39658046ef
GermanStevssel <germancstevssel@gmail.com>
2022-01-25 19:18:38 -0300

    estableciendo las rutas solicitadas

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const { Router } = express;
+const router = Router();
 const PORT = 8080;
 const fs = require("fs");
 
@@ -89,10 +91,12 @@ const container = new Contenedor("./products.txt");
 // 		"https://cdn1.epicgames.com/b4565296c22549e4830c13bc7506642d/offer/TETRA_PREORDER_STANDARD%20EDITION_EPIC_Store_Landscape_2560x1440-2560x1440-827a9d1823ad230a0ea5a2efc7936370.jpg?h=270&resize=1&w=480",
 // });
 
-console.log(container.getAll());
+// console.log(container.getAll());
 // container.getById(1);
 // container.deleteById(2);
 // container.deleteAll();
+
+// Desafío 3
 
 // se indica el puerto en el cual nuestra aplicación reciba las peticiones
 // Colocando 0, express selecciona un puerto libre al azar
@@ -100,31 +104,73 @@ const server = app.listen(PORT, () => {
 	console.log(`Express is listening in port http://localhost:${PORT}`);
 });
 
-/* El método .on() sirve para indicar un error en la puessta en marcha del servidor, sobre
+/* El método .on() sirve para indicar un error en la puesta en marcha del servidor, sobre
  * la salida de .listen()
  */
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
-app.all("/", function (request, response) {
-	response.send(
-		"<h1>Desafío 3</h1><h2><a href='./productos'>Productos</h2><h2><a href='./producto-random'>Producto random</h2>"
-	);
-});
+// app.all("/", function (request, response) {
+// 	response.send(
+// 		"<h1>Desafío 3</h1><h2><a href='./productos'>Productos</h2><h2><a href='./producto-random'>Producto random</h2>"
+// 	);
+// });
 
 // Para obtener info del server, utilizamos la petición GET
 // Obtener todos los productos en un array
-app.get("/productos", (req, res) => {
+// app.get("/productos", (req, res) => {
+// 	res.send(container.getAll());
+// });
+
+// Obtener un producto random
+// app.get("/producto-random", (req, res) => {
+// 	const maxProducts = container.getAll().length + 1;
+// 	const productId = Math.floor(Math.random() * (maxProducts - 1)) + 1;
+// 	const product = container.getById(productId);
+// 	res.send(`<div>
+// 		<h1>${product.title}</h1>
+// 		<h2>$ ${product.price}</h2>
+// 		<img src="${product.thumbnail}" alt="Imagen de videjouego" />
+// 	</div>`);
+// });
+
+// Desafío 4
+
+app.use("/api", router);
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+router.get("/productos", (req, res) => {
 	res.send(container.getAll());
 });
 
-// Obtener un producto random
-app.get("/producto-random", (req, res) => {
-	const maxProducts = container.getAll().length + 1;
-	const productId = Math.floor(Math.random() * (maxProducts - 1)) + 1;
+router.get("/producto/:productId", (req, res) => {
+	const productId = parseInt(req.params.productId);
 	const product = container.getById(productId);
 	res.send(`<div>
 		<h1>${product.title}</h1>
 		<h2>$ ${product.price}</h2>
 		<img src="${product.thumbnail}" alt="Imagen de videjouego" />
 	</div>`);
+});
+
+router.post("/productos", (req, res) => {
+	container.save(req.body);
+	res.json(req.body);
+});
+
+router.put("/productos/:id", (req, res) => {
+	const idProvided = Number(req.params.id);
+	container.updateById(idProvided, {
+		...req.body,
+		id: idProvided,
+	});
+	res.send(container.getById(idProvided));
+});
+
+router.delete("/productos/:id", (req, res) => {
+	const idProvided = Number(req.params.id);
+	res.send(container.deleteById(idProvided));
 });
