8462b271335f9e52562d7cd442940c283fcae629
GermanStevssel <germancstevssel@gmail.com>
2022-02-17 11:46:15 -0300

    cambios para primer entrega de proyecto final

--- api.js
+++ api.js
@@ -1,27 +1,27 @@
 import fs from "fs";
 import Contenedor from "./templates/ejs/container.js";
-const container = new Contenedor("./products.txt");
 import express, { json, urlencoded } from "express";
 import { Server as HttpServer } from "http";
 import { Server as IOServer } from "socket.io";
+const container = new Contenedor("./products.txt");
 const app = express();
 const httpServer = new HttpServer(app); //le paso mis datos de app para levantar la aplicación
 const io = new IOServer(httpServer); //establezco mi puente de datos en RT con el servidor http
 const { Router } = express;
 const router = Router();
+const videogamesRouter = Router();
+const cartRouter = Router();
 const PORT = 8080;
 
 app.use("/center", router); // las rutas de router inician con /center/....
-const server = httpServer.listen(PORT, () => {
-	console.log(`Express is listening in port http://localhost:${PORT}`);
-});
-
+app.use("/center/productos", videogamesRouter);
+app.use("/center/cart", cartRouter);
 app.use(express.static("public"));
 
-server.on("error", (error) => console.log(`Error en servidor ${error}`));
-
-router.use(json());
-router.use(urlencoded({ extended: true }));
+videogamesRouter.use(json());
+videogamesRouter.use(urlencoded({ extended: true }));
+cartRouter.use(json());
+cartRouter.use(urlencoded({ extended: true }));
 
 app.set("view engine", "ejs"); // registra el motor de plantillas
 app.set("views", "./public/views"); // especifica el directorio de vistas
@@ -35,25 +35,19 @@ io.on("connection", (socket) => {
 	console.log("Nuevo cliente conectado");
 	//Envío los mensajes existentes a todos los nuevos clientes
 	socket.emit("ServerMsgs", messages);
-
 	socket.emit("videogames", videogames);
-
 	socket.on("newVideogame", (data) => {
 		let newID = videogames.length + 1;
-
 		let newTitle = data.title;
 		let newPrice = data.price;
 		let newThumbnail = data.thumbnail;
-
 		const videogame = {
 			id: newID,
 			title: newTitle,
 			price: newPrice,
 			thumbnail: newThumbnail,
 		};
-
 		videogames.push(videogame);
-
 		io.sockets.emit("videogames", videogames);
 	});
 	/*Escucho los mensajes enviados por el cliente con nombre "clientMsg" y lo broadcasteo 
@@ -71,41 +65,155 @@ io.on("connection", (socket) => {
 	});
 });
 
-// Endpoints
+let admin = true;
+const authError = {error: -1, description: "No posee los permisos para llevar adelante esta acción"}
+// Endpoints Videogames
 app.get("/", (req, res) => {
 	res.render("index", { videogames });
 });
 
-router.get("/productos", (req, res) => {
+videogamesRouter.get("/videogames", (req, res) => {
 	res.send(container.getAll());
 });
 
-router.get("/productos/:productId", (req, res) => {
-	const productId = parseInt(req.params.productId);
-	const product = container.getById(productId);
+videogamesRouter.get("/videogames/:videogameId", (req, res) => {
+	const videogameId = parseInt(req.params.videogameId);
+	const videogame = container.getById(videogameId);
 	res.send(`<div>
-	<h1>${product.title}</h1>
-	<h2>$ ${product.price}</h2>
-	<img src="${product.thumbnail}" alt="Imagen de videjouego" />
+	<h1>${videogame.title}</h1>
+	<h2>$ ${videogame.price}</h2>
+	<img src="${videogame.thumbnail}" alt="Imagen de videjouego" />
 	</div>`);
 });
 
-router.post("/productos", (req, res) => {
-	container.save(req.body);
-	videogames.push(req.body);
-	res.redirect("/");
+videogamesRouter.post("/videogames", (req, res) => {
+	admin ? (container.save(req.body);
+					videogames.push(req.body);
+					res.redirect("/");) 
+					: 
+					(res.send(authError))
 });
 
-router.put("/productos/:productId", (req, res) => {
-	const productId = parseInt(req.params.productId);
-	container.updateById(productId, {
-		...req.body,
-		id: productId,
-	});
-	res.send(container.getById(productId));
+videogamesRouter.put("/videogames/:videogameId", (req, res) => {
+	const videogameId = parseInt(req.params.videogameId);
+	admin ? (container.updateById(videogameId, {
+						...req.body,
+						id: videogameId,
+					}) 
+					res.send(container.getById(videogameId));)
+					:
+					res.send(authError)
 });
 
-router.delete("/productos/:productId", (req, res) => {
-	const productId = parseInt(req.params.productId);
-	res.send(container.deleteById(productId));
+videogamesRouter.delete("/videogames/:videogameId", (req, res) => {
+	const videogameId = parseInt(req.params.videogameId);
+	admin ? res.send(container.deleteById(videogameId)) : res.send(authError)
 });
+
+// Endpoints Cart
+
+const carts = []; // cartArrays
+
+const videogamesColection = []; //productsArray
+
+const date = new Date();
+
+const userError = {error: -2, description: "Usuario no encontrado"}
+
+cartRouter.get("/:userId/videogames", (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const userCart = carts.filter(
+    (cart) => cart.id === userId
+  );
+
+	if (userCart.length > 0) {
+		userCart[0].videogames.length > 0 ? res.send(userCart[0].videogames) : { products: "No hay productos en este carrito" }
+	} else {
+		res.send(userError)
+	}
+});
+
+cartRouter.post("/", (req, res) => {
+  if (carts.length === 0) {
+    const newCart = {
+      id: 1,
+      cartTimeStamp: date.toLocaleString(),
+      vieogames: [],
+    };
+    carts.push(newCart);
+    res.send(`Nuevo carrito creado, su númer de id es: ${newCart.id.toString()}`);
+  } else {
+    const indexOfLastElement = carts.length - 1;
+    const newCart = {
+      id: carts[indexOfLastElement].id + 1,
+      cartTimeStamp: date.toLocaleString(),
+      vieogames: [],
+    };
+    carts.push(newCart);
+    res.send(`Nuevo carrito creado, su númer de id es: ${newCart.id.toString()}`);
+  }
+
+	const textCarts = JSON.stringify(carts)
+  fs.writeFileSync("./carts.txt", textCarts);
+});
+
+cartRouter.post("/:userId/productos", (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const indexOfCart = carts.findIndex(
+    (cart) => cart.id === userId
+  );
+
+	indexOfCart > -1 ? (videogamesColection.push(req.body);
+   										carts[indexOfCart].videogames = videogamesColection;
+    									res.redirect("/");)
+											: 
+											(res.send(userError))
+  const textCarts = JSON.stringify(carts)
+  fs.writeFileSync("./carts.txt", textCarts);
+});
+
+cartRouter.delete("/:userId", (req, res) => {
+  const userId = Number(req.params.userId);
+  const indexOfCart = carts.findIndex(
+    (cart) => cart.id === userId
+  );
+
+  indexOfCart > -1 ? (carts.splice(indexOfCart, 1);
+    									res.redirect("/");)
+											:
+											(res.send(userError);)
+  const textCarts = JSON.stringify(carts) 
+  fs.writeFileSync("./carts.txt", textCarts);
+});
+
+cartRouter.delete("/:userId/productos/:videogameId", (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const videogameId = parseInt(req.params.videogameId);
+
+  const indexOfCart = carts.findIndex(
+    (cart) => cart.id === userId
+  );
+	
+  if (indexOfCart > -1) {
+		const videogamesInCart = carts[indexOfCart].videogames;
+    const videogameIndex = videogamesInCart.findIndex(
+      (videogame) => videogame.id === videogameId
+    );
+    if (videogameIndex > -1) {
+			videogamesInCart.splice(videogameIndex, 1);
+      res.send(carts);
+    } else {
+     res.send(userError);
+    }
+  } else {
+		res.send(userError);
+  }
+	const textCarts = JSON.stringify(carts) 
+  fs.writeFileSync("./carts.txt", textCarts);
+});
+
+const server = httpServer.listen(PORT, () => {
+	console.log(`Express is listening in port http://localhost:${PORT}`);
+});
+
+server.on("error", (error) => console.log(`Error en servidor ${error}`));
